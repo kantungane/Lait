d1b9bef2c225d2d06bad396812000b31817cd1a1
kantungane <jeannekantungane6@gmail.com>
2023-03-06 16:41:23 +0200

    creation du fichier Delete_VenteAuxAbonnes.php

---
+++ Delete/Delete_VenteAuxAbonnes.php
@@ -0,0 +1,14 @@
+<?php 
+
+require '../Side/database.php';
+$id=$_GET['id'];
+$delete=$pdo-> prepare("DELETE FROM Ventes WHERE Id_Vente=?");
+	$delete->execute(array($id));
+	if($delete){
+		
+		header('location:../Home/VenteAuxAbonnes.php');
+		exit;
+	}else{
+		echo "Erreur de suppression";
+	}
+ ?>
