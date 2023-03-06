55a61befc72f1af49e50b19ba9b90229b593dc8a
kantungane <jeannekantungane6@gmail.com>
2023-03-06 10:12:16 +0200

    creation du fichier Footer.php

---
+++ Footer.php
@@ -0,0 +1,38 @@
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title></title>
+			<meta charset="utf-8">
+			<style type="text/css">
+			
+		 #footer{width:100%;
+		background-image: radial-gradient(pink 5%,black);
+		height: 60px;
+		padding-top: 10px;
+		color:black;
+		position: absolute;
+	}
+				
+		</style>
+		</head>
+		<body>
+		
+	<center><div id ="footer">
+		
+		 		<tr style="marquee">
+		 				
+		 					<td >BUJUMBURA</td>
+		 					<td >Commune: MUHA</td>
+		 					<td >Quartier:KANYOSHA</td>
+		 					<td >&#128222;Tel:79 662 409 </td>
+		 					<td >&#9993;Email:kantunganejeanne6@gmail.com</td>
+		 					
+		 		</tr>				
+		 		
+			</table></center>
+	</div>
+	</center>
+	</body>
+	</html>
+
+		   	
