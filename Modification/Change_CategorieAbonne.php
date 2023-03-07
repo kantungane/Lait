816d7d77afe7a26943c794fe8cc339a49d0f87a9
kantungane <jeannekantungane6@gmail.com>
2023-03-07 11:50:28 +0200

    creation du fichier Change_CategorieAbonne.php

--- Modification/Change_CategorieAbonne.php
+++ Modification/Change_CategorieAbonne.php
@@ -5,7 +5,6 @@
 	<meta charset="utf-8">
 	<style type="text/css">
 		
-		
 		form{
 			width: 700px;
 			height:auto;
@@ -20,7 +19,7 @@
 <body>
 	<?php require "../Header.php";?>
 <div style=" background-image:radial-gradient(pink 10%,steelblue);margin-top: -20px;height: 40px; ">               
-	<h3 style="margin-left: 600px;">Formulaire de modification de categorie des Depenses</h3>
+	<h3 style="margin-left: 600px;">Formulaire de modification de categorie d'abonnement</h3></center>
 </div>
 <?php
 require("../Side/Sidebar.php");
@@ -33,13 +32,33 @@ require("../Side/database.php");
 	
 <label class="form-label">Nom de la Categorie</label><br>
 <input class="form-control" type="text" name="nom_category" id="categorie"><br><br>
-	
-<input class="form-control" type="submit" name="submit" value="Enregistrement" style="width:200px;background: teal; margin-left:300px; ">
+
+<label class="form-label">Nom du Produit</label><br>
+<select class="form-select" name="produit"id="produit">
+	<?php  $conn = $pdo ->prepare("SELECT * FROM Produits ");
+			$conn->execute(array());
+			$row=$conn->rowcount();
+			if($row>0){
+			
+			while($lign=$conn->fetch(PDO::FETCH_ASSOC))
+			 {
+			 	 ?>
+			 	<option> <?php echo $lign["Designation"]; ?></option>
+			 <?php	
+				}
+			}
+			 ?>
+</select>	
+
+<br><br>	
+<input class="form-control" type="submit" name="submit" value="Enregistrement" style="width:200px;background: teal; margin-left:300px; "><br><br>
 
 </form>
-<?php
+
+
+<?php 
 $id=$_GET['id'];
-		$compte=$pdo-> prepare("SELECT * FROM Categorie_Depenses WHERE Id_Categorie=?");
+		$compte=$pdo-> prepare("SELECT * FROM Categorie_Abonnes WHERE Id_Categorie=?");
 			$compte->execute(array($id));
 			$row =$compte->rowcount();
 			if($row > 0){
@@ -48,22 +67,22 @@ $id=$_GET['id'];
 				<script type="text/javascript">
 					function recharger(){
 						document.getElementById("categorie").value="<?php echo $ligne['Designation'];?>"
-					
+						document.getElementById("produit").value="<?php echo $ligne['Nom_Produit'];?>"
 						
 						}
 				</script>
 			<?php 
 			}
-		}                   
+		}                  
 if (isset($_POST['submit'])){
   $nom=htmlspecialchars($_POST['nom_category']);
-	
-       if ( !empty( $_POST['nom_category']) ){
-  $inser=$pdo->prepare("UPDATE Categorie_Depenses SET Designation=? WHERE Id_Categorie=?");
-   $inser->execute(array($nom,$id));
+  $produit=htmlspecialchars($_POST['produit']);
+       if ( !empty( $_POST['nom_category']) AND !empty( $_POST['produit']) ){
+  $inser=$pdo->prepare("UPDATE Categorie_Abonnes SET Designation=?,Nom_Produit=? WHERE Id_Categorie=?");
+   $inser->execute(array($nom,$produit,$id));
    
    	 $succes= "Modification réussie";
- 
+ 	
 
  }else{
  	$error= "completer tous les champs";
